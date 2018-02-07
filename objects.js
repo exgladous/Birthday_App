1435ca32e91d06c8fdd17f98a820181860201bc3
Matthew J Gao <mg00805@srcs.loc>
2018-02-07 12:14:13 -0800

    fixed some spaces, added days until next birthday

--- objects.js
+++ objects.js
@@ -9,5 +9,22 @@ function getAge() {
         birthdate = new Date(birthday),
         age = Math.floor((today - birthdate) / (365 * 24 * 60 * 60 * 1000));
 
-    return "You are" + age + "years of age."
+    return "You are " + age + " years of age. There are " + getDaysUntilNextBirthday() + " days until your next birthday!"
+}
+
+function getDaysUntilNextBirthday() {
+    var today = new Date,
+        nextyear = today.getYear() + 1,
+        birthday = document.getElementById("birthdayinput").value,
+        birthdate = new Date(birthday),
+
+        nextbirthday = new Date (birthdate.setFullYear(today.getFullYear()));
+
+    if (today - nextbirthday > 0) {
+        var nextbirthday = nextbirthday.setFullYear(today.getFullYear() + 1)
+    }
+
+        var daysuntilnextbirthday = Math.ceil((nextbirthday - today) / (24 * 60 * 60 * 1000))
+
+    return daysuntilnextbirthday
 }
