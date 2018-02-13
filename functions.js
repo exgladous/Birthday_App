74a4d636f819962a3950cc7705515bb41ad2f7f8
Danny  Lam <dl01258@srcs.loc>
2018-02-13 11:12:46 -0800

    changed stuff

--- functions.js
+++ functions.js
@@ -10,7 +10,7 @@ function getAge() {
         birthdate = new Date(birthday),
         age = Math.floor((today - birthdate) / (365 * 24 * 60 * 60 * 1000));
 
-    return "You are " + age + " years of age. There are " + getDaysUntilNextBirthday() + " days until your next birthday! " + "You are a(n) " + getHoroscope();
+    return "You are " + age + " years of age. There are " + getDaysUntilNextBirthday() + " days until your next birthday! " + "You are " + getHoroscope();
 }
 
 function getDaysUntilNextBirthday() {
@@ -39,40 +39,40 @@ function getHoroscope() {
 
     if ((days >= 20 && month == 0) || (days <= 18 && month == 1)) {
         document.getElementById("picture").src = "./Images/Aquarius.PNG";
-        return "Aquarius!";
+        return "an Aquarius!";
     } else if ((days >= 19 && month == 1) || (days <= 20 && month == 2)) {
         document.getElementById("picture").src = "./Images/Pisces.PNG";
-        return "Pisces!";
+        return "a Pisces!";
     } else if ((days >= 21 && month == 2) || (days <= 19 && month == 3)) {
         document.getElementById("picture").src = "./Images/Aries.PNG";
-        return "Aries!";
+        return "an Aries!";
     } else if ((days >= 21 && month == 3) || (days <= 20 && month == 4)) {
         document.getElementById("picture").src = "./Images/Taurus.PNG";
-        return "Taurus!";
+        return "a Taurus!";
     } else if ((days >= 21 && month == 4) || (days <= 22 && month == 5)) {
         document.getElementById("picture").src = "./Images/Gemini.PNG";
-        return "Gemini!";
+        return "a Gemini!";
     } else if ((days >= 23 && month == 5) || (days <= 22 && month == 6)) {
         document.getElementById("picture").src = "./Images/Cancer.PNG";
-        return "Cancer!";
+        return "a Cancer!";
     } else if ((days >= 23 && month == 6) || (days <= 22 && month == 7)) {
         document.getElementById("picture").src = "./Images/Leo.PNG";
-        return "Leo!";
+        return "a Leo!";
     } else if ((days >= 23 && month == 7) || (days <= 22 && month == 8)) {
         document.getElementById("picture").src = "./Images/Virgo.PNG";
-        return "Virgo!";
+        return "a Virgo!";
     } else if ((days >= 23 && month == 8) || (days <= 21 && month == 9)) {
         document.getElementById("picture").src = "./Images/Libra.PNG";
-        return "Libra!";
+        return "a Libra!";
     } else if ((days >= 22 && month == 9) || (days <= 21 && month == 10)) {
         document.getElementById("picture").src = "./Images/Scorpio.PNG";
-        return "Scorpio!";
+        return "a Scorpio!";
     } else if ((days >= 22 && month == 10) || (days <= 19 && month == 11)) {
         document.getElementById("picture").src = "./Images/Sagittarius.PNG";
-        return "Sagittarius!";
+        return "a Sagittarius!";
     } else if ((days >= 19 && month == 0) || (days <= 20 && month == 1)) {
         document.getElementById("picture").src = "./Images/Capricorn.PNG";
-        return "Capricorn!";
+        return "a Capricorn!";
     }
 }
 
